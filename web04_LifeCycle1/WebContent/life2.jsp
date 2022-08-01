25f656096ccbb3ae91e35f5fc9371fb5ef7c35ab
USERMIC-9H64N6C\BIFF2020 <dnjswnsthd@naver.com>
2022-08-01 13:52:28 +0900

    Add web04_LIfeCycle1

---
+++ web04_LifeCycle1/WebContent/life2.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body bgcolor='cyan'>
+	<h3>LifeCycle CallBack Method Test...Page 이동방법</h3>
+	<B>Count :: </B>
+	<%=request.getParameter("cnt")%>
+</body>
+</html>
--- web04_LifeCycle1/src/web/servlet/life/ServletLifeCycleTest2.java
+++ web04_LifeCycle1/src/web/servlet/life/ServletLifeCycleTest2.java
@@ -10,12 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // Annotation ---> XML
-//@WebServlet("/Life1")
-public class ServletLifeCycleTest1 extends HttpServlet {
+public class ServletLifeCycleTest2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private int count = 0;
 
-	public ServletLifeCycleTest1() { // Servlet 생성할때... Container가 DD일 읽은 후
+	public ServletLifeCycleTest2() { // Servlet 생성할때... Container가 DD일 읽은 후
 		System.out.println("1. 서블릿 객체가 생성...컨테이너에 의해서...");
 	}
 
@@ -42,12 +41,11 @@ public class ServletLifeCycleTest1 extends HttpServlet {
 		// 로직은 여기서 작성
 		System.out.println("3. servie() 호출 --> doGet()/doPost()재호출...컨테이너에 의해서...");
 		PrintWriter out = response.getWriter();
-		out.println("<html><body bgcolor='yellow'>");
-		out.println("<h3>LifeCycle CallBack Method....</h3>");
-		out.println("<b>Count :: </b>" + ++count);
-		out.println("</body></html>");
+		// 클라이언트의 서비스를 수행... 수행결과 데이터를 jsp페이저로 연결...
+		count = 1004;
+		out.println("<a href=life2.jsp?cnt=" + count + ">life2.jsp</a>");
+		// 브라우저로 일단 응답을 하지않고 ...서버안에서 바로 jsp로 연결....응답을 jsp가 한다.
+//		request.getRequestDispatcher("life2.jsp?cnt=" + count).forward(request, response);;
 		out.close();
-
 	}
-
 }
